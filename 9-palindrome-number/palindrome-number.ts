17081349eba691b61c3b67ed1b9e91b2b75697b2
Suergaz <thelonglqd@gmail.com>
2026-03-10 12:13:19 +0700

    Time: 4 ms (92.54%) | Memory: 64.7 MB (84.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.ts
+++ 9-palindrome-number/palindrome-number.ts
@@ -1,5 +1,21 @@
+// 2567
 function isPalindrome(x: number): boolean {
-    if (x < 0) return false
+    if (x === 0) return true;
+    if (x < 0 || x % 10 === 0) return false
+    if (x < 10) return true;
 
-    return x === parseInt((""+x).split("").reverse().join(""))
+    // return x === parseInt((""+x).split("").reverse().join(""))
+
+    let tmp = 0;
+    // let power = 0;
+    let remain = x;
+    // let remain = number;
+
+    while (remain >= 10) {
+        let lastDigit = remain % 10
+        tmp = tmp * 10 + lastDigit;
+        remain = (remain - lastDigit) / 10
+    }
+
+    return x === tmp * 10 + remain;
 };
