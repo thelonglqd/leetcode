ecf1d3cd88aa4487e73186f32efda8eb3198523f
Suergaz <thelonglqd@gmail.com>
2026-03-11 15:20:23 +0700

    Time: 2 ms (84.78%) | Memory: 56.6 MB (90.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.ts
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -1,33 +1,21 @@
-const isCloseBrackets = (char) => [")", "}", "]"].includes(char);
-const isOpenBrackets = (char) => ["(", "{", "["].includes(char);
-
-const isPair = (a, b) => {
-  if (a === ')') return b === "("
-  else if (a === '(') return b === ")"
-  else if (a === '[') return b === ']'
-  else if (a === ']') return b === '['
-  else if (a === '{') return b === '}'
-  else return b === '{'
-}
+const parentheses = new Map();
+parentheses.set("(", ")")
+parentheses.set("[", "]")
+parentheses.set("{", "}")
 
 function isValid(s: string): boolean {
 
-const stack = [];
-
+    const stack = [];
+    
     if (s.length % 2 !== 0) return false;
 
     for (let i = 0; i < s.length; i++) {
-      if (isOpenBrackets(s[i])) stack.push(s[i])
+      if (parentheses.has(s[i])) stack.push(parentheses.get(s[i]))
       else {
-        if (isPair(s[i], stack[stack.length - 1])) {
-          stack.pop()
-        } else {
-          return false;
-        }
+        let last = stack.pop();
+        if (last !== s[i]) return false;
       }
     }
 
     return stack.length === 0;
 };
-
-// isValid("(([]){})")
