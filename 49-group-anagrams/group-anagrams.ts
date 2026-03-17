1f4bd7c04d836b5a907217b574745ca05511f08a
Suergaz <thelonglqd@gmail.com>
2026-03-17 15:34:49 +0700

    Time: 21 ms (92.41%) | Memory: 71 MB (8.26%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,24 +1,27 @@
-function generateKey(str) {
-    const tmp = new Array(26).fill(0)
-    
-    for (let i = 0; i < str.length; i++) {
-      tmp[str.charCodeAt(i) - 97]++
-    }
-  
-  return String.fromCharCode(...tmp)
-}
-
 function groupAnagrams(strs) {
-    const tmp = {};
-  
+    const res = {};
+    const count = new Uint8Array(26); // TypedArray nhanh và tiết kiệm hơn mảng thường
+
     for (let i = 0; i < strs.length; i++) {
-        const key = generateKey(strs[i])
-        if (tmp[key]) {
-          tmp[key].push(strs[i])
-        } else {
-            tmp[key] = [strs[i]]
+        const s = strs[i];
+        
+        // Đếm trực tiếp (Inline)
+        for (let j = 0; j < s.length; j++) {
+            count[s.charCodeAt(j) - 97]++;
         }
+
+        // Tạo key thủ công hoặc dùng String.fromCharCode cực nhanh
+        let key = "";
+        for (let k = 0; k < 26; k++) {
+            if (count[k] !== 0) {
+                key += String.fromCharCode(k + 97) + count[k];
+            }
+            count[k] = 0; // Reset mảng ngay tại đây để dùng cho từ sau
+        }
+
+        if (!res[key]) res[key] = [];
+        res[key].push(s);
     }
-  
-    return Object.values(tmp);
-};
+
+    return Object.values(res);
+}
